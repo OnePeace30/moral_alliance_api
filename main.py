61a2b4edd6ee5a63603296e47cc13f21bd2b1f86
Fallen <txpark74@gmail.com>
2024-04-10 10:49:26 +0000

    p

--- main.py
+++ main.py
@@ -36,14 +36,14 @@ meta_posts = {"posts":
             }
 
 all_points = {
-    # 'states': {"model": State, "serializer": StateSerializer, "how": "all"},
+    'states': {"model": State, "serializer": StateSerializer, "how": "all"},
     'universities': {"model": Universities, "serializer": UniversitiesSerializer, "how": "all", "filters": [Universities.done.is_(True)]},
     'articles': {"model": RelatedArticles, "serializer": RelatedArticlesSerializer, "how": "all", "order": RelatedArticles.date.desc()},
-    # 'groups': {"model": HateGroups, "serializer": HateGroupsSerializer, "how": "all"},
-    # 'gifts': {"model": Gifts, "serializer": GiftsSerializer, "how": "all", "filters": [Gifts.university_id.isnot(None)]},
-    # 'alumni': {"model": AlumniNotable, "serializer": AlumniNotableSeralizer, "how": "all"},
-    # 'incidents': {"model": EventsAnnotated, "serializer": EventsAnnotatedSerializer, "how": "all"},
-    # 'feed': {"model": Events, "serializer": EventsSerializer, "how": "all", "filters": [Events.uni_id.isnot(None)]}
+    'groups': {"model": HateGroups, "serializer": HateGroupsSerializer, "how": "all"},
+    'gifts': {"model": Gifts, "serializer": GiftsSerializer, "how": "all", "filters": [Gifts.university_id.isnot(None)]},
+    'alumni': {"model": AlumniNotable, "serializer": AlumniNotableSeralizer, "how": "all"},
+    'incidents': {"model": EventsAnnotated, "serializer": EventsAnnotatedSerializer, "how": "all"},
+    'feed': {"model": Events, "serializer": EventsSerializer, "how": "all", "filters": [Events.uni_id.isnot(None)]}
 }
 
 
